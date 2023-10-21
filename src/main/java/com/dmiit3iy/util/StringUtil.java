43df3dcd8fdda509dc8827dea326d41e28fd32c4
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2023-10-21 09:52:30 +0500

    добавил расчет хэша

---
+++ src/main/java/com/dmiit3iy/util/StringUtil.java
@@ -0,0 +1,14 @@
+package com.dmiit3iy.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+
+
+public class StringUtil {
+    public static String generateHash() {
+        int length = 10;
+        boolean useLetters = true;
+        boolean useNumbers = true;
+        return DigestUtils.md5Hex(RandomStringUtils.random(length, useLetters, useNumbers));
+    }
+}
